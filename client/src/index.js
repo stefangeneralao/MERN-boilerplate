749a74eb7fd7d99c341e90c9618350891336ab50
Stefan Generalao <stefan.generalao@gmail.com>
2018-12-30 17:44:30 +0100

    Fixed failing import path

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from 'components/App/App';
+import App from 'components/App/AppContainer';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
